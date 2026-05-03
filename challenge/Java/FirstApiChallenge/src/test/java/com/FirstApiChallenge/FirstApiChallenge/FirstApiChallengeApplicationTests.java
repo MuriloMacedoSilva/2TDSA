613761b2f74408fc5661c97ec06c76dc46a6416a
Murilo <murilo@192.168.15.46>
2026-05-03 12:55:20 -0300

    hhhhh

---
+++ challenge/Java/FirstApiChallenge/src/test/java/com/FirstApiChallenge/FirstApiChallenge/FirstApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstApiChallenge.FirstApiChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
